1f7cf09bf3769567cb3f14ae9f2c16d134053fd5
Jurgen <jurgen1582@yahoo.com.br>
2022-01-22 16:50:36 -0300

    initial commit

---
+++ Respostas/Boletos.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace Respostas
+{
+    class Boletos
+    {
+        double Valor { get; set; }
+        DateTime DataVencimento { get; set; }
+        DateTime NovaData { get; set; }
+
+
+        public Boletos()
+        {
+        }
+
+        internal object CalculaBoletos(double valorBoleto, DateTime dataNova, DateTime dataOriginal)
+        {
+            DateTime dNova = dataNova;
+            DateTime dvelha = dataOriginal;
+            double valorT = 0.0;
+            
+
+            var utils = new Utils();
+            var feriado = utils.GetHolidaysByCurrentYear(dvelha.Year);
+            bool findi = false;
+
+            foreach (var day in feriado)
+            {
+                if (dvelha == day && dvelha.DayOfWeek == DayOfWeek.Saturday)
+                    dvelha = dvelha.AddDays(2);
+                else if (dvelha == day && dvelha.DayOfWeek == DayOfWeek.Friday)
+                    dvelha = dvelha.AddDays(3);
+                else if(dvelha == day)
+                    dvelha = dvelha.AddDays(1);
+            }
+
+            if (dvelha.DayOfWeek == DayOfWeek.Sunday)
+            {
+                dvelha = dvelha.AddDays(1);
+                findi = true;
+            }                
+            if (dvelha.DayOfWeek == DayOfWeek.Saturday)
+            {
+                dvelha = dvelha.AddDays(2);
+                findi = true;
+            }
+
+            TimeSpan dataCob = dNova.Subtract(dvelha);
+            if (dvelha.DayOfWeek != DayOfWeek.Sunday || dvelha.DayOfWeek != DayOfWeek.Saturday)
+                 valorT = (dataCob.Days * 0.03) + valorBoleto;
+
+            if (findi != true && dataCob.Days >= 1)
+                valorT += 2;
+
+            return valorT;
+        }
+    }
+}
---
+++ Respostas/Resposta4.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Respostas
+{
+    class Resposta4
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Recalculo dos Boletos");
+            Console.WriteLine("Informe a Data de Vencimento(ORIGINAL). 00/00/00 ");
+            DateTime dataOriginal = DateTime.Parse(Console.ReadLine());
+            Console.WriteLine("Informe a Data de Vencimento(NOVA). 00/00/00 ");
+            DateTime dataNova = DateTime.Parse(Console.ReadLine());
+            Console.WriteLine("Informe o valor do Boleto: ");
+            double valorBoleto = double.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            Boletos boletos = new Boletos();
+
+            double valorNovo = (double)boletos.CalculaBoletos(valorBoleto, dataNova, dataOriginal);
+
+            var valorJuros = valorNovo - valorBoleto;
+
+            Console.WriteLine("Valor do Boleto Recalculado: " + valorNovo);
+            Console.WriteLine("Valor do Juros sobre o Boleto: " + valorJuros);
+        }
+    }
+}
